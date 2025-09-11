27b867455b5aba81124c363f22cadbd8b6fc0daa
Halimunnisa Shaik <shaikmunni1314@gmail.com>
2025-09-11 20:43:35 +0530

    Update main.jsx

--- food-delivery-app/src/main.jsx
+++ food-delivery-app/src/main.jsx
@@ -4,8 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";  // <-- add this line
-import "bootstrap/dist/js/bootstrap.bundle.min.js";  // <-- add this line
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 
